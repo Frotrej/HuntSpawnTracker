ded4c4a50e9308f977bf56dc7112463ed0df255f
seba300 <49442098+seba300@users.noreply.github.com>
2023-05-21 14:46:01 +0200

    Prepare DbContext, CodeFirst

---
+++ WpfApp1/Infrastructure/HuntContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Infrastructure.Models;
+
+namespace WpfApp1.Infrastructure
+{
+    public class HuntContext:DbContext
+    {
+        public HuntContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<Map> Maps { get; set; }
+        public DbSet<Location> Locations { get; set; }
+    }
+}
---
+++ WpfApp1/Infrastructure/Models/Map.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Infrastructure.Models
+{
+    public class Map
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int MapOccurrences { get; set; }
+
+        public virtual List<Location> Locations { get; set; }
+    }
+}
